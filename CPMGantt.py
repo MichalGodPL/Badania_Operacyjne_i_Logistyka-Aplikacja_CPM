ec32998e6801cf36746237d888caf51e0dce1a68
Michał Szymański <michalszymanskyy@gmail.com>
2025-03-15 21:01:43 +0100

    Aplikacja CPM - Alfa 10 - Czarny Gantt, Czarny Gantt

--- CPMGantt.py
+++ CPMGantt.py
@@ -1,23 +1,49 @@
 import matplotlib.pyplot as plt
+
 import pandas as pd
+
 import matplotlib.dates as mdates
 
-def visualize_gantt_chart(tasks):
-    df = pd.DataFrame(tasks)
-    df['start'] = pd.to_datetime(df['earliest_start'], unit='D')
-    df['end'] = df['start'] + pd.to_timedelta(df['duration'], unit='D')
 
-    fig, ax = plt.subplots(figsize=(12, 6))
+def visualize_gantt_chart(tasks):
+
+    df = pd.DataFrame(tasks)
+
+    df['start'] = df['earliest_start']
+
+    df['end'] = df['start'] + df['duration']
+
+
+    fig, ax = plt.subplots(figsize=(12, 6), facecolor='black')
+
+    ax.set_facecolor('black')
+
+    for spine in ax.spines.values():
+
+        spine.set_color('white')
+
+    ax.tick_params(axis='x', colors='white')
+
+    ax.tick_params(axis='y', colors='white')
+
+    ax.set_xlabel('Czas (dni)', color='white')
+
+    ax.set_ylabel('Zadania', color='white')
+
+    ax.set_title('Wykres Gantta', color='white')
+
+    ax.grid(True, color='white', linestyle='--', alpha=0.3)
+
+
     for idx, row in df.iterrows():
+
         ax.barh(row['name'], row['duration'], left=row['start'], color='skyblue')
 
-    ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
     plt.gca().invert_yaxis()
-    plt.xlabel('Czas')
-    plt.ylabel('Zadania')
-    plt.title('Wykres Gantta')
-    plt.grid(True)
-    plt.savefig("gantt_chart.png")
+
+    plt.savefig("gantt_chart.png", facecolor='black')
+
     plt.close()
+    
     return "gantt_chart.png"
